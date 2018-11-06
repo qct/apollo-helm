e14fc4667b736838a69f5e565e081dec2daf3943
qct <tango.qct@gmail.com>
2018-11-06 06:26:29 +0000

    Polish run scripts

--- hack/dry-run.sh
+++ hack/dry-run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-helm install --dry-run --debug --name apollo --namespace wrd-infra ../ \
+helm install --dry-run --debug --name apollo --namespace wrd-infra ./ \
              --set portalmysql.persistence.storageClass="local-storage" \
              --set configservicemysql.persistence.storageClass="local-storage" \
              --set portal.ingress.enabled=true \
              --set portal.ingress.hosts={apollo-portal.k8s-dev.com}
+#             --set configservice.extraEnv."eureka\.service\.url"="http://apollo-configservice:8080/eureka/" \
+#             --set configservice.extraEnv."logging\.level\.root"=debug
--- hack/run.sh
+++ hack/run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-helm install --name apollo --namespace wrd-infra ../ \
+helm install --name apollo --namespace wrd-infra ./ \
              --set portalmysql.persistence.storageClass="local-storage" \
              --set configservicemysql.persistence.storageClass="local-storage" \
              --set portal.ingress.enabled=true \
              --set portal.ingress.hosts={apollo-portal.k8s-dev.com}
+#             --set configservice.extraEnv."eureka\.service\.url"="http://apollo-configservice:8080/eureka/" \
+#             --set configservice.extraEnv."logging\.level\.root"=debug
+
