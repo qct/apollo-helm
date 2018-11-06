26121f4a5aa3c228af59674fd92734c96167c7c2
qct <tango.qct@gmail.com>
2018-11-06 06:59:39 +0000

    Set two environments(DEV,PRO) by default

--- hack/dry-run.sh
+++ hack/dry-run.sh
@@ -2,8 +2,11 @@
 
 helm install --dry-run --debug --name apollo --namespace wrd-infra ./ \
              --set portalmysql.persistence.storageClass="local-storage" \
-             --set configservicemysql.persistence.storageClass="local-storage" \
+             --set devconfigservicemysql.persistence.storageClass="local-storage" \
+             --set proconfigservicemysql.persistence.storageClass="local-storage" \
              --set portal.ingress.enabled=true \
              --set portal.ingress.hosts={apollo-portal.k8s-dev.com}
+#             --set fatconfigservicemysql.persistence.storageClass="local-storage" \
+#             --set uatconfigservicemysql.persistence.storageClass="local-storage" \
 #             --set configservice.extraEnv."eureka\.service\.url"="http://apollo-configservice:8080/eureka/" \
 #             --set configservice.extraEnv."logging\.level\.root"=debug
